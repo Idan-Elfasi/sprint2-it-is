4bc1096fe63c06e8d367b00ff285ff9f01cc052c
Idan Elfasi <idan60167@gmail.com>
2024-03-20 21:43:04 +0200

    deliverty 21:30

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -4,6 +4,14 @@ function renderGallery() {
     var getImgs = ""
     getImgs = `<img onclick="onImgSelect (this)"  src="img/2.jpg">`
     gGallery.innerHTML += getImgs
+    // var strHtmls = gImgs.map(img => `
+    // // <article class="img">
+    // //     <img src="${img.url}" 
+    // //     alt="Image ${img.id}"
+    // //     onclick="onImgSelect(${img})">
+    // // </article> 
+    // // `)
+
 
 }
 function onImgSelect (img){
--- js/meme.service.js
+++ js/meme.service.js
@@ -4,14 +4,25 @@ const gPxPadding = gFontSize * 1.5
 const gpxPaddingInline = gFontSize * 0.5
 const gpxPaddingBottom = gFontSize * 0.2
 
-var gborderData = {
-  x: 0,
-  y: 0,
-  width: 0,
-  hight: 0
-}
+
 var gIds = 1
-var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gImgs = [{ id: 1, url: 'img/2.jpg', keywords: ['funny', 'cat'] },
+{ id: 2, url: 'img/3.jpg', keywords: ['animals', 'cute'], },
+{ id: 3, url: 'img/4.jpg', keywords: ['funny', 'animals', 'cute'] },
+{ id: 4, url: 'img/5.jpg', keywords: ['animals'] },
+{ id: 5, url: 'img/6.jpg', keywords: ['funny', 'sad', 'cute'] },
+{ id: 6, url: 'img/7.jpg', keywords: ['funny', 'crazy'] },
+{ id: 7, url: 'img/8.jpg', keywords: ['funny', 'sarcastic'] },
+{ id: 8, url: 'img/9.jpg', keywords: ['happy', 'sarcastic'] },
+{ id: 9, url: 'img/10.jpg', keywords: ['funny', 'happy', 'cute'] },
+{ id: 10, url: 'img/11.jpg', keywords: ['happy'] },
+{ id: 11, url: 'img/12.jpg', keywords: ['guilty'] },
+{ id: 12, url: 'img/13.jpg', keywords: ['success'] },
+{ id: 13, url: 'img/14.jpg', keywords: ['guilty'] },
+{ id: 14, url: 'img/15.jpg', keywords: [] },
+{ id: 15, url: 'img/16.jpg', keywords: ['happy'] },
+{ id: 16, url: 'img/17.jpg', keywords: ['guilty'] },
+{ id: 17, url: 'img/18.jpg', keywords: ['happy', 'sad'] }]
 var gMeme = {
   selectedImgId: 1,
   selectedLineIdx: 0,
@@ -53,7 +64,7 @@ function ChangeTxtColor(inputColor) {
 
 function DecreaseSizeByBtn() {
   gMeme.lines[gMeme.selectedLineIdx].size += -0.05
-  
+
 }
 function IncreaseSizeByBtn() {
   gMeme.lines[gMeme.selectedLineIdx].size += 0.05
@@ -105,7 +116,7 @@ function addBorderTxt() {
 
   var mull = 1.42857 // line hight from bootstrap 
   var size = gMeme.lines[gMeme.selectedLineIdx].size * gFontSize
-  
+
   var borderHieght = (mull * size)
 
 
@@ -114,10 +125,10 @@ function addBorderTxt() {
 
   gCtx.strokeRect(x, y - 0.5 * borderHieght, widthBorder, borderHieght * 0.5 + gpxPaddingBottom)
 
-gborderData={
-x,
-y:y - 0.5 * borderHieght,
-width:widthBorder + gpxPaddingInline,
-hight:borderHieght * 0.5 + gpxPaddingBottom
-}
+  gborderData = {
+    x,
+    y: y - 0.5 * borderHieght,
+    width: widthBorder + gpxPaddingInline,
+    hight: borderHieght * 0.5 + gpxPaddingBottom
+  }
 }
